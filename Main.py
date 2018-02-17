d58afb79edcf68a30619d4db6c831df1e0ca76cd
AYGIT1 <AYGIT1@users.noreply.github.com>
2018-02-17 19:52:09 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,6 +11,8 @@ from PyQt5.QtWidgets import (QWidget, QLabel,
 
 BaseCurrSet = set()
 MarketCurrSet = set()
+#DataTup = ()
+#TODO: Do not use global variables
 
 def DataPull():
     try:
@@ -19,10 +21,18 @@ def DataPull():
         print("Connection to the server could not be established.")
 
 
-
     for Shovel in DataRaw.json()["result"]:
         BaseCurrSet.add(Shovel["BaseCurrency"])
         MarketCurrSet.add(Shovel["MarketCurrency"])
+
+    # DataTup = BaseCurrSet, MarketCurrSet
+    # print(DataTup)
+    # for Code in DataTup[0]:
+    #     print(type(Code))
+    #     print(Code)
+
+#def DataRefine():
+
 
 class MainWindow(QWidget):
 
@@ -36,30 +46,32 @@ class MainWindow(QWidget):
     ####### Base Currency Menu #######
         self.BasLbl = QLabel("Select a base currency", self)
         BaseCombo = QComboBox(self)
-        BaseCombo.addItem("Ubuntsdgu")
-        BaseCombo.addItem("Mandriva")
+        BaseCombo.addItem("-")
+        for Code in BaseCurrSet:
+            BaseCombo.addItem(Code)
         self.BasLbl.move(35, 25)
         BaseCombo.move(35, 50)
-        BaseCombo.activated[str].connect(self.onActivated)
+        # BaseCombo.activated[str].connect(self.onActivated)
 
     ####### Market Currency Menu #######
         self.MarLbl = QLabel("Select a market currency", self)
         MarketCombo = QComboBox(self)
-        MarketCombo.addItem("Osman ")
-        MarketCombo.addItem("Hayri")
-        self.MarLbl.move(150, 25)
-        MarketCombo.move(150, 50)
-        MarketCombo.activated[str].connect(self.onActivated)
+        MarketCombo.addItem("-")
+        for Code in MarketCurrSet:            #TODO: Bad UX, should be in alphabetical order
+            MarketCombo.addItem(Code)
+        self.MarLbl.move(180, 25)
+        MarketCombo.move(180, 50)
+        # MarketCombo.activated[str].connect(self.onActivated)
 
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(300, 300, 600, 400)
         self.setWindowTitle('QComboBox')
         self.show()
 
-    def onActivated(self, text):
-        self.BasLbl.setText(text)
-        self.BasLbl.adjustSize()
-        self.MarLbl.setText(text)
-        self.MarLbl.adjustSize()
+    #  def onActivated(self, text):
+    #     self.BasLbl.setText(text)
+    #     self.BasLbl.adjustSize()
+    #     self.MarLbl.setText(text)
+    #     self.MarLbl.adjustSize()
 
 
 if __name__ == '__main__':

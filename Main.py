a48126d7225f7b518b318ccda3c09d5e07e3ec2a
AYGIT1 <AYGIT1@users.noreply.github.com>
2018-02-22 06:17:48 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,15 @@
 #
-#A little more than simple program to monitor cryptocurrency market values. Data pulled from bittrex.com
+# A little more than simple program to monitor cryptocurrency market values. Data pulled from bittrex.com
 #
 #
 
 import sys
 import requests
-from PyQt5.QtWidgets import (QMainWindow, QWidget, QLabel, QComboBox, QApplication)
-
+from PyQt5.QtWidgets import (QWidget, QLabel, QComboBox, QApplication, QPushButton, QMessageBox)
 
 BaseCurrSet = set()
 MarketCurrSet = set()
-#TODO: Do not use global variables
+# TODO: Do not use global variables
 
 def data_pull():
     try:
@@ -18,68 +17,65 @@ def data_pull():
     except:
         print("Connection to the server could not be established.")
 
-
     for Shovel in data_raw.json()["result"]:
         BaseCurrSet.add(Shovel["BaseCurrency"])
         MarketCurrSet.add(Shovel["MarketCurrency"])
-
-def data_refine():
-
-    #Base = Get string from self.BasLbl
-    #Market = Get string from self.MarLbl
-    #exchange = "Base" + "Market"
-
-    try:
-        data_marketsum = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
-    except:
-        print("Connection to the server could not be established.")
-
-    for Bucket in data_marketsum.json()["result"]:
-        if Bucket['MarketName'] == exchange:
-            rates = [int(Bucket['High'), int(Bucket['Low']), int(Bucket['Last']),]
-            
-    return rates
 
 
 class AppMain(QWidget):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
 
-
-        ####### Base Currency Menu #######
+        # Base Currency Menu #######
         self.BasLbl = QLabel("Select a base currency", self)
-        BaseCombo = QComboBox(self)
-        BaseCombo.addItem("-")
+        self.BaseCombo = QComboBox(self)
+        self.BaseCombo.addItem("-")
         for Code in BaseCurrSet:
-            BaseCombo.addItem(Code)
-        self.BasLbl.move(35, 25)
-        BaseCombo.move(35, 50)
-        # BaseCombo.activated[str].connect(self.onActivated)
+            self.BaseCombo.addItem(Code)
+        self.BasLbl.move(40, 25)
+        self.BaseCombo.move(40, 50)
+        self.BaseCombo.currentTextChanged.connect(self.base_change_name)
 
-        ####### Market Currency Menu #######
+        # Market Currency Menu #######
         self.MarLbl = QLabel("Select a market currency", self)
-        MarketCombo = QComboBox(self)
-        MarketCombo.addItem("-")
-        for Code in MarketCurrSet:            #TODO: Bad UX, should be in alphabetical order
-            MarketCombo.addItem(Code)
+        self.MarketCombo = QComboBox(self)
+        self.MarketCombo.addItem("-")
+        for Code in MarketCurrSet:  # TODO: Bad UX, should be in alphabetical order
+            self.MarketCombo.addItem(Code)
         self.MarLbl.move(180, 25)
-        MarketCombo.move(180, 50)
-        # MarketCombo.activated[str].connect(self.onActivated)
+        self.MarketCombo.move(180, 50)
+        self.MarketCombo.currentTextChanged.connect(self.market_change_name)
+
+        # self.btn = QPushButton('Button', self)
+        # self.btn.resize(self.btn.sizeHint())
+        # self.btn.move(40, 150)
+        #
+        # self.LastVal = QLabel("Last Value: " ,self)
+        # self.LastVal.move(130, 160)
+        # self.LastVal.adjustSize()
 
         self.setGeometry(300, 300, 600, 400)
         self.setWindowTitle('QComboBox')
         self.show()
 
-    #  def onActivated(self, text):
-    #     self.BasLbl.setText(text)
-    #     self.BasLbl.adjustSize()
-    #     self.MarLbl.setText(text)
-    #     self.MarLbl.adjustSize()
+    def base_change_name(self):
+
+
+    def market_change_name(self):
+
+
+    # def closeEvent(self, event):
+    #
+    #     reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+    #
+    #     if reply == QMessageBox.Yes:
+    #         event.accept()
+    #     else:
+    #         event.ignore()
 
 
 if __name__ == '__main__':
@@ -87,3 +83,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = AppMain()
     sys.exit(app.exec_())
+
